8a64e3c3cb6ef94985190d73c9ea9bb8faf8163d
Ezequiel74452 <ezequielcaviglione@hotmail.com>
2021-08-25 22:58:37 -0300

    Launcher preparado

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,21 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	Student IEGC = new Student(74452, "García Caviglione", "Isaí Ezequiel", "ezequielcaviglione@hotmail.com", "https://github.com/Ezequiel74452", "https://avatars.githubusercontent.com/u/60025474?v=4");
+            	try {
+            		SimplePresentationScreen sps = new SimplePresentationScreen(IEGC);
+            		sps.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
             }
         });
     }
